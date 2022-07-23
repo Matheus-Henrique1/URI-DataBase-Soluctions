4f2d7171d4f49c8b16f692a3270946d13ca681c9
Matheus Henrique <m_peu@hotmail.com>
2022-07-23 02:40:25 -0300

    Solução do problema 2624 - Quantidades de Cidades por Clientes

---
+++ 2624 - Quantidades de Cidades por Clientes.sql
@@ -0,0 +1,10 @@
+/*
+A diretoria da empresa pediu para você um relatório simples de quantas
+cidades a empresa já alcançou.
+
+Para fazer isso você deve exibir a quantidade de cidades distintas da tabela clientes.
+*/
+
+SELECT COUNT(DISTINCT c.city) FROM customers c
+
+
